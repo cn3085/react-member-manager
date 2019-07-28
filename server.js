80cf9c3106bae2ad4cab90b32a1960994d2c7da7
LTJ <cn3085@gmail.com>
2019-07-28 18:50:04 +0900

    add customer delete

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const upload = multer({dest:'./upload'});
 
 app.get('/api/customers',(req,res)=>{
     connection.query(
-        "SELECT * FROM customer",
+        "SELECT * FROM customer where isDeleted = 0",
         (err, rows, fields) => {
             console.log('----------------------------------------');
             console.log(rows);
@@ -37,7 +37,7 @@ app.get('/api/customers',(req,res)=>{
 
 app.use('/image', express.static('./upload'));
 app.post('/api/customers',upload.single('image'), (req,res) =>{
-    let sql = 'INSERT INTO customer VALUES (null, ?, ?, ?, ?, ?)';
+    let sql = 'INSERT INTO customer VALUES (null, ?, ?, ?, ?, ?, now(), 0)';
     let image = '/image/'+req.file.filename;
     let name = req.body.name;
     let birthday = req.body.birthday;
@@ -49,4 +49,15 @@ app.post('/api/customers',upload.single('image'), (req,res) =>{
     })
     
 })
+
+app.delete('/api/customers/:id', (req,res) => {
+    let sql = 'UPDATE customer set isDeleted = 1 where id = ?';
+    let params = [req.params.id];
+    connection.query(sql, params,
+        (err, rows, feilds) => {
+            res.send(rows);
+        })
+})
+
+
 app.listen(port,() => console.log('server start'));
